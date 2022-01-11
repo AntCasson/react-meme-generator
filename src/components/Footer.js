57530a6f0b9a07e1f82d80a1bd2e9d9fcbfb34a4
AntCasson <anthonyrcasson@gmail.com>
2022-01-11 20:32:40 +0100

    Changed the styling and file order src and public

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+        <p>Made by <span>AntCasson</span></p>
+        <a 
+          href="https://github.com/AntCasson">
+            <img
+              src="../images/GitHub.png" 
+              alt="github logo"/> 
+        </a>
+    </footer>
+  )
+}
